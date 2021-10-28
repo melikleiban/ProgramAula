1386bedd49094e33d630495b162a4e0c422f461d
Jose Villena <josevillena@live.com>
2021-10-27 23:01:10 -0300

    Adaptadas las entidades a Lombok. Agregado correctamente el nombre de la base de datos en application.yml

--- src/main/java/proyect/entidades/Foto.java
+++ src/main/java/proyect/entidades/Foto.java
@@ -1,7 +1,4 @@
-package proyect.entidades;
-
-import java.util.Arrays;
-import java.util.Objects;
+package proyect.Entidades;
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
@@ -12,68 +9,22 @@ import javax.persistence.Lob;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Foto {
 	
 	@Id
 	@GeneratedValue(generator = "uuid")
 	@GenericGenerator(name = "uuid", strategy = "uuid2")
 	private String id;	
+	
 	private String nombre;
+	
 	private String mime;
+	
 	@Lob @Basic(fetch = FetchType.LAZY)
 	private byte[] contenido;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getMime() {
-		return mime;
-	}
-	public void setMime(String mime) {
-		this.mime = mime;
-	}
-	public byte[] getContenido() {
-		return contenido;
-	}
-	public void setContenido(byte[] contenido) {
-		this.contenido = contenido;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Arrays.hashCode(contenido);
-		result = prime * result + Objects.hash(id, mime, nombre);
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Foto other = (Foto) obj;
-		return Arrays.equals(contenido, other.contenido) && Objects.equals(id, other.id)
-				&& Objects.equals(mime, other.mime) && Objects.equals(nombre, other.nombre);
-	}
-	@Override
-	public String toString() {
-		return "Foto [id=" + id + ", nombre=" + nombre + ", mime=" + mime + ", contenido=" + Arrays.toString(contenido)
-				+ "]";
-	}
 	
-	
-	
-
 }
--- src/main/java/proyect/entidades/Usuario.java
+++ src/main/java/proyect/entidades/Usuario.java
@@ -1,108 +1,38 @@
-package proyect.entidades;
+package proyect.Entidades;
 
-import java.util.Objects;
+
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.GenericGenerator;
 
-
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 
-@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Usuario {
 	
 	@Id
 	@GeneratedValue(generator="uuid")
 	@GenericGenerator(name="uuid", strategy="uuid2")
-	private String id;
+	protected String id;
+	
 	@NonNull
 	@Column(nullable=false,unique=true)
-	private String nombreUsuario;
-	private String nombreCompleto;
-	private String email;
-	private String telefono;
-	private String localidad;
+	protected String nombreUsuario;
+	
+	protected String nombreCompleto;
+	protected String email;
+	protected String telefono;
+	protected String localidad;
+	
 	@NonNull
-	private String contraseña;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-	}
-	public String getNombreCompleto() {
-		return nombreCompleto;
-	}
-	public void setNombreCompleto(String nombreCompleto) {
-		this.nombreCompleto = nombreCompleto;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-	public String getLocalidad() {
-		return localidad;
-	}
-	public void setLocalidad(String localidad) {
-		this.localidad = localidad;
-	}
-	public String getContraseña() {
-		return contraseña;
-	}
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(contraseña, email, id, localidad, nombreCompleto, nombreUsuario, telefono);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(contraseña, other.contraseña) && Objects.equals(email, other.email)
-				&& Objects.equals(id, other.id) && Objects.equals(localidad, other.localidad)
-				&& Objects.equals(nombreCompleto, other.nombreCompleto)
-				&& Objects.equals(nombreUsuario, other.nombreUsuario) && Objects.equals(telefono, other.telefono);
-	}
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nombreUsuario=" + nombreUsuario + ", nombreCompleto=" + nombreCompleto
-				+ ", email=" + email + ", telefono=" + telefono + ", localidad=" + localidad + ", contraseña="
-				+ contraseña + "]";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	protected String contraseña;
 
 }
