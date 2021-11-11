7e99c1cb239c395440cabc2ce5e0344e4498d604
Meli Kleiban <meli.kleiban@gmail.com>
2021-11-11 17:49:08 -0300

    Configuración seguridad

--- src/main/java/proyect/Configuraciones/ConfiguracionSeguridad.java
+++ src/main/java/proyect/Configuraciones/ConfiguracionSeguridad.java
@@ -16,7 +16,7 @@ import proyect.Servicios.UsuarioServicio;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@Order(1000)                                                        
+@Order(99)                                                        
 public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
@@ -31,20 +31,10 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.headers().frameOptions().sameOrigin().and()
-		.authorizeRequests()
-		.antMatchers("/css/*","/js/*","/img/*")
-		.permitAll()
-		.and().formLogin()
-		.loginPage("/login")
-		.loginProcessingUrl("/logincheck")
-		.usernameParameter("username")
-		.passwordParameter("password")
-		.defaultSuccessUrl("/")
-		.permitAll()
-		.and().logout()
-		.logoutUrl("/logout")
-		.logoutSuccessUrl("/")
-		.permitAll();
+		http.authorizeRequests().antMatchers("/css/*", "/js/*", "/img/*", "/*").permitAll().and().formLogin()
+				.loginPage("/login").loginProcessingUrl("/logincheck").usernameParameter("email")
+				.passwordParameter("clave").defaultSuccessUrl("/loginsuccess").failureUrl("/login?error=error")
+				.permitAll().and().logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll().and().csrf()
+				.disable();
 	}
 }
