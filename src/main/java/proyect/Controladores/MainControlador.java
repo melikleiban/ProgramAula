145ff0611185fd47b7b6fafaa29de8f013a7f7bb
Meli Kleiban <meli.kleiban@gmail.com>
2021-11-11 22:01:22 -0300

    comentarios

--- src/main/java/proyect/Controladores/MainControlador.java
+++ src/main/java/proyect/Controladores/MainControlador.java
@@ -1,8 +1,10 @@
 package proyect.Controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -14,7 +16,10 @@ public class MainControlador {
 	}
 	
 	@GetMapping("/login")
-	public String login() {
+	public String login(@RequestParam(required = false) String error, ModelMap modelo) {
+		if(error!=null) {
+			modelo.put("error", "Contrasenia incorrecta");
+		}
 		return "login.html";
 	}
 	
