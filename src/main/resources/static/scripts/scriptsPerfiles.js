013c40891dff6b7364313b33db9cec4b9a741a01
Sol Piqueras <piquerasmariasol@gmail.com>
2021-11-12 13:45:54 -0300

    Añado archivos perdidos...

---
+++ src/main/resources/static/scripts/scriptsPerfiles.js
@@ -0,0 +1,25 @@
+jQuery('document').ready(function($){
+
+    var menuBtn = $('.menu-icon'),
+    menu = $('.navigation ul');
+
+    menuBtn.click(function() {
+        if(menu.hasClass('show')) {
+            menu.removeClass('show');
+        } else {
+            menu.addClass('show');
+        }
+
+    });
+});
+
+jQuery('document').ready(function($) {
+	var editBtn = $('#edit1');
+	editBtn.click(function() {
+		if(document.getElementById("inputDireccion").readOnly = false){
+		 	document.getElementById("inputDireccion").readOnly = true;
+		 }else{
+			document.getElementById("inputDireccion").readOnly = false;
+		}
+	});
+});
